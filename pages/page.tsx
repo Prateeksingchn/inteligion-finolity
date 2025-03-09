f5f6fbac83b658189888254b7bceef5b3a7980a6
Prateek Singh Chouhan <prateeksinghchouhan4499@gmail.com>
2025-03-09 18:33:50 +0530

    fix deployment error

--- pages/page.tsx
+++ pages/page.tsx
@@ -1,20 +1,37 @@
-import LatestNews from '../components/layout/LatestNews'; // Adjust the path as necessary
-import UpcomingEvents from '../components/layout/UpcomingEvents'; // Adjust the path as necessary
-import RecognitionSection from '../components/layout/RecognitionSection'; // Adjust the path as necessary
-import CompanyRecognize from '@/components/layout/CompanyRecognize';
+'use client';
 
-export default function Page() {
-    return (
-        <div>
-            {/* Other components like Industries */}
-            <Industries />
-            <LatestNews /> {/* Add the Latest News section here */}
-            <UpcomingEvents /> {/* Add the Upcoming Events section here */}
-            <VendorsSection />
-            <RecognitionSection /> {/* Add the Recognition Section here */}
-            <TestimonialsSection />
-            <CompanyRecognize />
-            {/* Other components */}
-        </div>
-    );
-} 
+import { Header } from '@/components/layout/Header';
+import { Hero } from '@/components/layout/Hero';
+import { MetricsAndServices } from '@/components/layout/MetricsAndServices'
+import Solution from "@/components/layout/Solution";
+import Industries from "@/components/layout/Industries";
+import LatestNews from '@/components/layout/LatestNews';
+import UpcomingEvents from '@/components/layout/UpcomingEvents';
+import VendorsSection from '@/components/layout/VendorsSection';
+import RecognitionSection from '@/components/layout/RecognitionSection';
+import SuccessStoriesSection from '@/components/layout/SuccessStoriesSection';
+import TestimonialsSection from '@/components/layout/TestimonialsSection';
+import CompanyRecognize from '@/components/layout/CompanyRecognize';
+import { Footer } from '@/components/layout/Footer';
+import ContactSection from '@/components/layout/ContactSection';
+
+export default function Home() {
+  return (
+    <div className="min-h-screen">
+      <Header />
+      <Hero />
+      <MetricsAndServices />
+      <Solution />
+      <Industries />
+      <LatestNews /> 
+      <UpcomingEvents />
+      <VendorsSection />
+      <RecognitionSection /> 
+      <SuccessStoriesSection />
+      <TestimonialsSection />
+      <CompanyRecognize />
+      <ContactSection />
+      <Footer />
+    </div>
+  );
+}
